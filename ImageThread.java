431566007c8e6b3102132a89e6a52e584a165b27
Robert Gruener <rjgruener@gmail.com>
2014-02-11 19:23:10 -0500

    update

---
+++ ImageThread.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.io.*;
+
+public class ImageThread extends Thread {
+    private Socket socket = null;
+
+    public ImageThread(Socket socket) {
+        super("ImageThread");
+        this.socket = socket;
+    }
+
+    public void run() {
+
+        try (
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                    socket.getInputStream()));
+        ) {
+            String inputLine, outputLine;
+            KnockKnockProtocol kkp = new KnockKnockProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye"))
+                    break;
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
